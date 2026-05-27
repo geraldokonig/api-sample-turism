738fc277706eef515bee8bfa92ca21553aaba598
luciano.ferretto <luciano.ferretto@atitus.edu.br>
2026-05-27 20:48:51 -0300

    Zoo Digital - Código base para a atividade avaliativa

--- SegParte_Aula01/Codes/br/edu/atitus/Main.java
+++ SegParte_Aula01/Codes/br/edu/atitus/Main.java
@@ -2,6 +2,10 @@ package br.edu.atitus;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.edu.atitus.interfaces.Corrida;
+import br.edu.atitus.interfaces.Nado;
+import br.edu.atitus.interfaces.Voo;
+
 public class Main {
 void main() {
 
@@ -19,6 +23,8 @@ void main() {
     animais.add(flipper);
 
     listarAnimais(animais);
+
+    IO.println("Números de Animais no Final: " + Animal.getContador());
 }
 
 void listarAnimais(List<Animal> animais) {
@@ -27,5 +33,24 @@ void listarAnimais(List<Animal> animais) {
         animal.comer(); // De qual classe o método comer() será invocado?
                         // Depende do tipo concreto do objeto
         animal.emitirSom();
+        
+        if (animal instanceof Corrida) {
+            Corrida animalCorredor = (Corrida) animal;
+            animalCorredor.correr();
+        }
+        if (animal instanceof Nado) {
+            Nado animalNadador = (Nado) animal;
+            animalNadador.nadar();
+        }
+        if (animal instanceof Voo) {
+            Voo animalVoador = (Voo) animal;
+            animalVoador.voar();
+        }
     }
-}}
+}
+
+
+
+
+
+}
